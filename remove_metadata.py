67f3eb3ae320832027320ff528199acd20759017
Peter LeCavalier <pele6150@colorado.edu>
2024-05-07 18:26:04 -0600

    Modified removing metadata

--- remove_metadata.py
+++ remove_metadata.py
@@ -6,11 +6,13 @@ from glob import glob
 import os
 from tqdm import tqdm
 
-fps = glob("./scripts/parsed/tagged/*.txt")
+MOVIES_FP = "."
+
+fps = glob(f"{MOVIES_FP}/scripts/parsed/tagged/*.txt")
 
 for fp in tqdm(fps):
     with open(fp, encoding="utf-8") as tagged_file:
-        new_file = open(f"./scripts/parsed/final/{fp.split(os.sep)[-1][:-10]}final.txt", "a", encoding='utf-8')
+        new_file = open(f"{MOVIES_FP}/scripts/parsed/final/{fp.split(os.sep)[-1][:-10]}final.txt", "a", encoding='utf-8')
         for line in tagged_file:
             if line[:2] != "M:":
                 new_file.write(line)
